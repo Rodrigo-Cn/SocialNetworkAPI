d528fa501a5c57504b8bbdd8a8293f22f6d313b2
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2024-11-20 08:28:30 -0300

    feat: Adiciona as migrations da aplicação

---
+++ database/migrations/2024_11_19_141716_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->bigInteger('user_sender_id');
+            $table->bigInteger('user_receiver_id');
+
+            $table->foreign('user_sender_id')->references()->on('user')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_receiver_id')->references()->on('user')->onDelete('cascade')->onUpdate('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
